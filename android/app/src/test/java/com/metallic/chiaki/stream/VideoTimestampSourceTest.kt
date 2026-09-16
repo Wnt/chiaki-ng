1983d5d6f35af000c26bfc5fa38b14cef80721ce
Jonni Madekivi <jonni@madekivi.fi>
2026-09-16 15:34:37 +0000

    PLE-237: decouple video timestamp source from pacing

---
+++ android/app/src/test/java/com/metallic/chiaki/stream/VideoTimestampSourceTest.kt
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: LicenseRef-AGPL-3.0-only-OpenSSL
+
+package com.metallic.chiaki.stream
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class VideoTimestampSourceTest
+{
+	@Test fun unpacedWithoutRealPtsUsesSyntheticCounter()
+	{
+		assertEquals(VideoTimestampSource.SYNTHETIC_COUNTER,
+			videoTimestampSource(realVideoTimestamps = false, pacingEnabled = false))
+	}
+
+	@Test fun pacedWithoutRealPtsUsesSyntheticCounter()
+	{
+		assertEquals(VideoTimestampSource.SYNTHETIC_COUNTER,
+			videoTimestampSource(realVideoTimestamps = false, pacingEnabled = true))
+	}
+
+	@Test fun unpacedWithRealPtsUsesFrameIndex()
+	{
+		assertEquals(VideoTimestampSource.FRAME_INDEX,
+			videoTimestampSource(realVideoTimestamps = true, pacingEnabled = false))
+	}
+
+	@Test fun pacedWithRealPtsUsesFrameIndex()
+	{
+		assertEquals(VideoTimestampSource.FRAME_INDEX,
+			videoTimestampSource(realVideoTimestamps = true, pacingEnabled = true))
+	}
+}
